175c8502c8fdb11a3ab4c1bd55d88512a941933b
Amos Langat <amos.langat@student.moringaschool.com>
2022-08-18 22:35:30 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback){
+    callback();
+}
+receivesAFunction(callback);
+
+function returnsANamedFunction(){
+    let callback = function(){
+
+    };
+    return callback;
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){};
+}
